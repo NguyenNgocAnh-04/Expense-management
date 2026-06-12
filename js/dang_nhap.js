96de5ca4c631c3ad069d01b0e875a3fa99ac5ad4
Anh <anhnn.d25.ptit@gmail.com>
2026-06-12 18:55:27 +0700

    dang nhap dang ky done

--- js/dang_nhap.js
+++ js/dang_nhap.js
@@ -1,23 +1,20 @@
 //khai báo các biến//
 const tenDangNhap = document.getElementById("tenDangNhap");
 const matKhau = document.getElementById("mk");
-console.log("B1")
-const buttonDangNhap = document.getElementById("buttonDangNhap");
-console.log(buttonDangNhap);
+
 
 //lấy dữ liệu người dùng nhập//
-buttonDangNhap.addEventListener("click", function(){
-    console.log("B2")
+buttonDangNhap.addEventListener("click", function(event){
+    event.preventDefault();
+    
     let ten = tenDangNhap.value ;
     let mk = matKhau.value;
-    console.log("B3")
-    let duLieu = localStorage.getItem("taiKhoan")
-    console.log("B4")
     
-    console.log("B5");
+    let duLieu = localStorage.getItem("taiKhoan")
+   
     //ktra xem có tk hay chưa//
     if(duLieu === null){
-        console.log("B6");
+        
         alert("Chưa có tài khoản mất rùi, đăng ký nhé!");
         return;
     };
@@ -25,17 +22,16 @@ buttonDangNhap.addEventListener("click", function(){
     
     //So sánh dữ liệu đn với dữ liệu đki//
     if(
-        ten === taiKhoan.tenDangNhap && mk === taiKhoan.mk
+        ten === taiKhoan.tenDangNhap && mk === taiKhoan.matKhau
     ){
-        console.log("B7");
+        
         alert("Đăng nhập thành công");
         console.log("B8");
         window.location.href = "trang_chu.html"; //Đn thành công chuyển sang trang chủ//
     }
     else{
-        console.log("B9");
+        
         alert("ohoh! Sai tài khoản hoặc mật khẩu mất rồi!")
     };
-
-    
+   
 });
